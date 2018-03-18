8c708bd8093b1123dd49ca98bf2f3cd707492297
Paul Zietsman <phzietsman@gmail.com>
2018-03-18 14:55:43 +0200

    Code compiling

--- contracts/Battleships.sol
+++ contracts/Battleships.sol
@@ -3,10 +3,9 @@ pragma solidity ^0.4.19;
 contract Battleships {
 	
 	struct PlayerState {
-		string[] moves;
-		string[] hits;
+		uint[] moves;
 		uint boardHash;
-		uint[10] boardPositions;
+		uint[10] boatPositions;
 		string salt;
 	}
 
@@ -17,29 +16,49 @@ contract Battleships {
 		uint playerOneStateId;
 		uint playerTwoStateId;
 		
+		uint round;
+
 		string gameState; // States: OPEN, LOADING, INROUND, WAITINGEVAL, GG
 		address winner;
+		string outcome; 	// DRAW, WIN, WIN_DEFAULT, DEFAULT
   }
 
-	event NewGameAvailableEvent(uint _gameId);
-	event GameJoinedEvent(uint _gameId);
-
+	// Game Constants
 	uint public maxBoardSize = 10;
+	uint public maxGameLengthInRounds = 30;
+	uint public maxWaitTimeInHours = 12;
 
+	// Game State
 	Game[] public games;
 	PlayerState[] public playerStates;
-
 	mapping(address => uint[]) public playerGames;
+
+	// Funds
+	uint public tournamentFund = 0;
+	
+
+	// Game Events
+	event NewGameAvailableEvent(uint _gameId);
+	event GameJoinedEvent(uint _gameId);
+	event GameRoundStartedEvent(uint _gameId);
+
 
 	function createGame() public returns(uint) {
 		
-		PlayerState memory playerOneState = PlayerState(new string[](0),new string[](0),0,"");
-		PlayerState memory playerTwoState = PlayerState(new string[](0),new string[](0),0,"");
-
+		PlayerState memory playerOneState;
+		
 		uint playerOneStateId = playerStates.push(playerOneState);
-		uint playerTwoStateId = playerStates.push(playerTwoState);
-
-		Game memory newGame = Game(msg.sender, 0x00000000, playerOneStateId,playerTwoStateId,"OPEN", 0x00000000);
+		
+		Game memory newGame = Game({
+			playerOne: msg.sender,
+			playerTwo: 0x00000000,
+			playerOneStateId: playerOneStateId,
+			playerTwoStateId: 0,
+			round:0,
+			gameState:"OPEN",
+			winner: 0x00000000,
+			outcome: ""
+		});
 
 		uint gameId = games.push(newGame);
 		playerGames[msg.sender].push(gameId);
@@ -52,15 +71,37 @@ contract Battleships {
 		
 		require(keccak256(games[_gameId].gameState) == keccak256("OPEN"));
 		
+		PlayerState memory playerTwoState;
+		uint playerTwoStateId = playerStates.push(playerTwoState);
+
 		Game storage game = games[_gameId];
 		game.playerTwo = msg.sender;
+		game.playerTwoStateId = playerTwoStateId;
 		game.gameState = "LOADING";
+
+		playerGames[msg.sender].push(_gameId);
 
 		GameJoinedEvent(_gameId);
 	}
 
 	function updateLoadingState(uint _gameId, uint _boardHash) public {
 		require(keccak256(games[_gameId].gameState) == keccak256("LOADING"));
+		require(msg.sender == games[_gameId].playerOne || msg.sender == games[_gameId].playerTwo);
+
+		PlayerState storage playerOneState = playerStates[games[_gameId].playerOneStateId];
+		PlayerState storage playerTwoState = playerStates[games[_gameId].playerOneStateId];
+
+		if ( msg.sender == games[_gameId].playerOne ) {
+			playerOneState.boardHash = _boardHash;
+		} else {
+			playerTwoState.boardHash = _boardHash;
+		}
+
+		if (playerOneState.boardHash != 0 && playerOneState.boardHash != 0) {
+			games[_gameId].round = 1;
+			games[_gameId].gameState = "INROUND";
+			GameRoundStartedEvent(_gameId);
+		}
 	}
 
 	// A position must be between zero and 99
@@ -75,7 +116,7 @@ contract Battleships {
 		return hash;
 	}
 
-	function testBoardValidity(uint[10] _positions) public pure returns(bool) {
+	function testBoardValidity(uint[10] _positions) public view returns(bool) {
 
 		uint maxPosition = (maxBoardSize * maxBoardSize) - 1;
 
